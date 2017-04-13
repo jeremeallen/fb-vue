5fabfa6b3e1075c17a638957674d7e2729ca8fa1
Jereme Allen <jereme.allen@wheniwork.com>
2017-04-13 15:30:03 -0500

    Add in auth code

---
+++ src/helpers/firebaseConfig.js
@@ -0,0 +1,9 @@
+
+export const config = {
+  apiKey: "AIzaSyBMpfqE1kEF7qMl33b1FEVKIfvWu9EHwXU",
+  authDomain: "fb-vue.firebaseapp.com",
+  databaseURL: "https://fb-vue.firebaseio.com",
+  projectId: "fb-vue",
+  storageBucket: "fb-vue.appspot.com",
+  messagingSenderId: "803776686309"
+};
